4b9a5bf1229eadc28b8a8a793ee2fabddc48ef9c
zx <493089901@qq.com>
2021-09-28 11:55:31 +0800

    second commit

--- src/main/java/com/gittest/demo/DemoApplication.java
+++ src/main/java/com/gittest/demo/DemoApplication.java
@@ -11,6 +11,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("aaa");
         System.out.println("bbbddd");
+        System.out.println("iiiii");
     }
 
 }
